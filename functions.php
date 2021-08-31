18d6de449ba0e0eed9aa18d9342a579f7feae8e2
adamalcantara <alcantaradevelopment@gmail.com>
2021-08-31 10:02:10 -0400

    Title updating dynamically based on user options in Wordpress

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function followandrew_theme_support() {
+    //Adds dynamic title tag support
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme','followandrew_theme_support');
+
+
 function followandrew_register_styles() {
 
     $version = wp_get_theme()->get( 'Version' );
